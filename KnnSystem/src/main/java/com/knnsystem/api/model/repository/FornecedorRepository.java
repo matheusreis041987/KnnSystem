8ce96ce346f5ae49e258df6e7128d4ad7e8caefc
vsantsal <vsantsal@gmail.com>
2024-02-07 08:55:46 -0300

    fix: correcao na recuperacao de fornecedor

--- KnnSystem/src/main/java/com/knnsystem/api/model/repository/FornecedorRepository.java
+++ KnnSystem/src/main/java/com/knnsystem/api/model/repository/FornecedorRepository.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.br.CNPJ;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.knnsystem.api.model.entity.Fornecedor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
     Optional<Fornecedor> findByCnpj(@CNPJ String cnpj);
 
+    @Query("select f from Fornecedor f where f.cnpj = ?1  or f.razaoSocial = ?2 or f.numControle = ?3")
     List<Fornecedor> findByCnpjOrRazaoSocialOrNumControle(
             @CNPJ String cnpj, String razaoSocial, String numControle);
 
